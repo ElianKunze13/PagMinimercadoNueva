764a6c07562c9b153b04e0134280220577060f19
Elian <Elian.E.K@hotmail.com>
2025-08-12 13:45:01 -0300

    fix: se corrigio Main.component

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [ CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
